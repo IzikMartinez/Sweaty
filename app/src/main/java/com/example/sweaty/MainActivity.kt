8d0b34a580c9d0750849ce0269de207eda2f2546
rafaysar <shaikhabdurrafay@gmail.com>
2020-11-06 17:06:17 -0600

    Added Fade In/Out Transition and fixed some things

--- app/src/main/java/com/example/sweaty/MainActivity.kt
+++ app/src/main/java/com/example/sweaty/MainActivity.kt
@@ -14,41 +14,57 @@ class MainActivity: AppCompatActivity() {
         val plateButton = findViewById<Button>(R.id.toPlateButton)
         plateButton?.setOnClickListener {
             startActivity(Intent(this, ToPlateActivity::class.java))
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+
         }
 
         val warmupButton = findViewById<Button>(R.id.warmupButton)
         warmupButton?.setOnClickListener {
             startActivity(Intent(this, ToWarmupActivity::class.java))
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+
         }
 
         val bodybuildingButton = findViewById<Button>(R.id.toExerciseButton)
         bodybuildingButton?.setOnClickListener {
             startActivity(Intent(this, ToBodybuildingActivity::class.java))
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+
         }
 
         val powerliftingButton = findViewById<Button>(R.id.toPowerLifting)
         powerliftingButton?.setOnClickListener {
             startActivity(Intent(this, ToPowerliftingActivity::class.java))
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+
         }
 
         val bmiButton = findViewById<Button>(R.id.toBmiButton)
         bmiButton?.setOnClickListener {
             startActivity(Intent(this, ToBmiActivity::class.java))
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+
         }
 
         val macrosButton = findViewById<Button>(R.id.toMacrosButton)
         macrosButton?.setOnClickListener {
             startActivity(Intent(this, ToMacroActivity::class.java))
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+
         }
 
 
         val journalButton = findViewById<Button>(R.id.toJournalButton)
         journalButton?.setOnClickListener {
             startActivity(Intent(this, ToJournalActivity::class.java))
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+
         }
         val timerButton= findViewById<Button>(R.id.toTimerButton)
         timerButton?.setOnClickListener {
             startActivity(Intent(this, ToTimerActivity::class.java))
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+
         }
 
 
