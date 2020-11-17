99757ac192b53566a2a9683e4aa552d3d8cad993
imartinez21 <320-imartinez21@users.noreply.gitlab.tamucc.edu>
2020-11-16 18:29:22 -0600

    Harris Benedict implemented in Macro Calculator activity

--- app/src/main/java/com/example/sweaty/activities/MacroActivity.kt
+++ app/src/main/java/com/example/sweaty/activities/MacroActivity.kt
@@ -1,6 +1,7 @@
 package com.example.sweaty.activities
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
@@ -13,36 +14,87 @@ class MacroActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.macro_activity)
 
-        val carbo = findViewById<TextView>(R.id.tvCarbOut)
-        val protein = findViewById<TextView>(R.id.tvProteinOut)
-        val fat = findViewById<TextView>(R.id.tvFatOut)
-        val calories = findViewById<TextView>(R.id.tvTotalCalories)
+        var gender = intent.getBooleanExtra("EXTRA_GENDER", false)
 
-        val rdOne  = findViewById<RadioButton>(R.id.rdOne)
-        val rdTwo = findViewById<RadioButton>(R.id.rdTwo)
+        val weight = findViewById<EditText>(R.id.etWeight).text.toString().toDouble()
+        val height = findViewById<EditText>(R.id.etHeight).text.toString().toDouble()
+        val age = findViewById<EditText>(R.id.etAge).text.toString().toDouble()
 
         val rdbMale = findViewById<RadioButton>(R.id.rdbMale)
+        gender = !rdbMale.isChecked
 
-        val rdbLowCarb  = findViewById<RadioButton>(R.id.rdbLowCarb)
-        val rdbNormal = findViewById<RadioButton>(R.id.rdbNormal)
+        val btnBioNext = findViewById<Button>(R.id.btnBioNext)
 
-        
+        btnBioNext.setOnClickListener() {
+            Intent(this, MacrosSecondActivity::class.java).also {
+                it.putExtra("EXTRA_WEIGHT", weight)
+                it.putExtra("EXTRA_HEIGHT", height)
+                it.putExtra("EXTRA_AGE", age)
+                it.putExtra("EXTRA_GENDER", gender)
+                startActivity(it)
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            }
+        }
 
+    }
+}
+
+class MacrosSecondActivity() : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.macro_second_activity)
+        val weight = intent.getDoubleExtra("EXTRA_WEIGHT", 60.0)
+        val height = intent.getDoubleExtra("EXTRA_HEIGHT", 180.0)
+        val age = intent.getIntExtra("EXTRA_AGE", 20)
+        val gender = intent.getBooleanExtra("EXTRA_GENDER", false)
+
+        val rdOne = findViewById<RadioButton>(R.id.rdOne)
+        val rdTwo = findViewById<RadioButton>(R.id.rdTwo)
+
+        val activity = when {
+            rdOne.isChecked -> 2
+            rdTwo.isChecked -> 3
+            else -> 4
+        }
+
+        val btnToRatio = findViewById<Button>(R.id.btnDaysWeekNext)
+
+        btnToRatio.setOnClickListener() {
+            Intent(this, MacrosThirdActivity::class.java).also {
+                it.putExtra("EXTRA_ACTIVITY", activity)
+                it.putExtra("EXTRA_WEIGHT", weight)
+                it.putExtra("EXTRA_HEIGHT", height)
+                it.putExtra("EXTRA_AGE", age)
+                it.putExtra("EXTRA_GENDER", gender)
+                startActivity(it)
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            }
+        }
+    }
+}
+
+class MacrosThirdActivity() : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.macro_third_activity)
+
+        val weight = intent.getDoubleExtra("EXTRA_WEIGHT", 60.0)
+        val height = intent.getDoubleExtra("EXTRA_HEIGHT", 180.0)
+        val age = intent.getDoubleExtra("EXTRA_AGE", 20.0)
+        val activity = intent.getIntExtra("EXTRA_ACTIVITY", 1)
+
+        val gender = intent.getBooleanExtra("EXTRA_GENDER", false)
         val btnGetMacros = findViewById<Button>(R.id.btnGetMacros)
 
+        val carbs = findViewById<TextView>(R.id.tvCarbOut)
+        val protein = findViewById<TextView>(R.id.tvProteinOut)
+        val fat = findViewById<TextView>(R.id.tvFatOut)
+        val calories = findViewById<TextView>(R.id.tvCalOut)
+
+        val rdbLowCarb = findViewById<RadioButton>(R.id.rdbLowCarb)
+        val rdbNormal = findViewById<RadioButton>(R.id.rdbNormal)
 
         btnGetMacros.setOnClickListener() {
-            val weight = findViewById<EditText>(R.id.etWeight).text.toString().toDouble()
-            val height = findViewById<EditText>(R.id.etHeight).text.toString().toDouble()
-            val age = findViewById<EditText>(R.id.etAge).text.toString().toDouble()
-
-            val activity = when {
-                rdOne.isChecked -> 2
-                rdTwo.isChecked -> 3
-                else -> 4
-            }
-
-            val gender = !rdbMale.isChecked
 
             val ratio = when {
                 rdbLowCarb.isChecked -> 0
@@ -53,14 +105,11 @@ class MacroActivity : AppCompatActivity() {
             val hb = HarrisBenedict()
             val macros = hb.getMacros(weight, height, age, activity, ratio, gender)
 
-            carbo.text = "Carbo: ${macros.first.toInt()}g"
+            carbs.text = "Carbo: ${macros.first.toInt()}g"
             protein.text = "Protein: ${macros.second.toInt()}g"
             fat.text = "Fat: ${macros.third.toInt()}g"
             calories.text = "Daily Calories: ${macros.first.toInt() *4 + macros.second.toInt()*4 + 
                     macros.third.toInt() * 9}"
         }
-
-
-
     }
 }
